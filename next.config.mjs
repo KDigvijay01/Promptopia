8cec6091c58cf167b031323eaef6abea5d62654c
Digvijay Singh <digvijaysingh@Digvijays-MacBook-Air.local>
2024-02-18 21:08:03 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      domains: ['lh3.googleusercontent.com', 'localhost'],
+    },
+    webpack(config) {
+      config.experiments = {
+        ...config.experiments,
+        topLevelAwait: true,
+      }
+      return config
+    }
+  }
+  
+  export default {...nextConfig};
+  
